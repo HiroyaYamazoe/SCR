d45f5996603825d7afef70a3f23f0d60653abd9b
HiroyaYamazoe <iwtbsos26@gmail.com>
2023-02-05 00:13:31 +0900

    add details and data

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' Example of the dendrite data
+#' 
+#' An Example of the dendrite data for 2d plot. This is converted from dendrite data in the library "spatstat".
+#' 
+#' @format ## `dendrite_ex`
+#' A data frame with 52 rows and 2 columns.
+#' These are xy-coordinates of the data.
+#' 
+"dendrite_ex"
+
+#' Example of The artificial data
+#' 
+#' An Example of the artificial data with t for 3d plot used in the paper.
+#' 
+#' @format "" `three_dimension_ex_with_t`
+#' A data frame with 600 rows and 4 columns.
+#' The 1st to 3rd columns are the xyz-coordinates of the data.
+#' The 4th column is the corresponding covariate.
+"three_dimension_ex_with_t"
